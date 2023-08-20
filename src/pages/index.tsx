4da94303c2c82c8f914f626a099e8c1b701af81c
Simon Eich <simoneich@MacBook-Air-von-Simon.local>
2023-08-20 12:52:33 +0200

    title white

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        
+        <h1 className="text-teal-100">Fantasy Judo - Make the sport personal</h1>
       </main>
     </>
   );
